47bf2e586c3ad55cd4e53ec4fa1ded245b63381a
Markus Ryöti <mryoti@gmail.com>
2026-02-01 10:52:57 +0200

    start firecracker process in rust

--- run-firecracker.sh
+++ run-firecracker.sh
@@ -3,7 +3,8 @@ API_SOCKET="/tmp/firecracker.socket"
 # Remove API unix socket
 sudo rm -f $API_SOCKET
 
-firecracker="/home/markusryoti/Downloads/firecracker-v1.14.1-x86_64/release-v1.14.1-x86_64"
+# Read this dynamically from 
+firecracker=$(pwd)/firecracker
 
 # Run firecracker
 sudo ./firecracker --api-sock "${API_SOCKET}" --enable-pci
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::process::Command;
+
+fn remove_existing_socket() {
+    Command::new("sudo")
+        .arg("rm")
+        .arg("-f")
+        .arg("/tmp/firecracker.socket")
+        .status()
+        .expect("Failed to remove socket with sudo");
+}
+
 fn main() {
-    println!("Hello, world!");
+    let current_dir = std::env::current_dir().expect("Failed to get current directory");
+    println!("Current directory: {}", current_dir.display());
+
+    remove_existing_socket();
+
+    let firecracker_path = current_dir.join("firecracker");
+    let child = Command::new(&firecracker_path)
+        .current_dir(current_dir)
+        .arg("--api-sock")
+        .arg("/tmp/firecracker.socket")
+        .arg("--enable-pci")
+        .spawn()
+        .expect("Failed to start firecracker");
+
+    println!("firecracker started with PID: {}", child.id());
+
+    child
+        .wait_with_output()
+        .expect("Failed to wait on firecracker process");
 }
