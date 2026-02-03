95e967632b14f10a92c741a2362426b7ba22c212
Markus Ryöti <mryoti@gmail.com>
2026-02-03 17:44:44 +0200

    build rootfs with docker

---
+++ tools/build-rootfs-image.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+cp target/debug/init rootfs/init
+
+docker build -t vm-rootfs rootfs
+
+container_id=$(docker create vm-rootfs)
+docker export "$container_id" | tar -C rootfs -xvf -
+docker rm "$container_id"
+
+# Create ext4 image
+dd if=/dev/zero of=rootfs.ext4 bs=1M count=128
+mkfs.ext4 rootfs.ext4
+sudo mount rootfs.ext4 /mnt
+sudo cp -r rootfs/* /mnt
+sudo umount /mnt
