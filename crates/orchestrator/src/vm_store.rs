cfbbc6f2085bb2ca5ca17e2567ed61b3b54359b4
Markus Ryöti <mryoti@gmail.com>
2026-02-17 17:53:05 +0200

    some mutexing

--- crates/orchestrator/src/vm_store.rs
+++ crates/orchestrator/src/vm_store.rs
@@ -1,27 +1,31 @@
-use std::sync::Arc;
+use std::{collections::HashMap, sync::Arc};
+
+use tokio::sync::Mutex;
 
 use crate::vm::VmConfig;
 
 pub struct VmStore {
-    vms: Vec<Arc<VmConfig>>,
+    vms: HashMap<String, Arc<Mutex<VmConfig>>>,
 }
 
 impl VmStore {
     pub fn new() -> Self {
-        VmStore { vms: Vec::new() }
+        VmStore {
+            vms: HashMap::new(),
+        }
     }
 
-    pub fn add_vm(&mut self, vm: Arc<VmConfig>) {
-        self.vms.push(vm);
+    pub fn add_vm(&mut self, id: &str, vm: VmConfig) {
+        self.vms.insert(id.into(), Arc::new(Mutex::new(vm)));
     }
 
-    pub fn get_vm(&self, id: &str) -> Option<&Arc<VmConfig>> {
-        let found = self.vms.iter().find(|vm| vm.id == id);
-        found
+    pub fn get_vm(&self, id: &str) -> Option<Arc<Mutex<VmConfig>>> {
+        let vm = self.vms.get(&id.to_string()).cloned();
+        vm
     }
 
     pub fn remove_vm(&mut self, id: &str) {
-        self.vms.retain(|vm| vm.id != id);
+        self.vms.remove(&id.to_string());
     }
 
     pub fn len(&self) -> usize {
